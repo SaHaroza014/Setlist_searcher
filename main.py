e8881c8c1771afc1468debada332761f2ad823a0
Sanjin Hadžalić <80854488+SaHaroza014@users.noreply.github.com>
2022-08-21 22:50:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = "dT-x7DQsZ0yR2GwewRgEcUBvTGiS8zTVEVYE"
+API_KEY = "dT-x7DQsZ0yR2GwewRgEcUBvTGiS8zTVEVYE"  # you can get your API_KEY on setlist.fm web page...
 
 artist_name = input("Input artist name: ")
 cityName = input("Input city name: ")
@@ -12,21 +12,26 @@ headers = {
 }
 response = requests.get(url, headers=headers)
 response.raise_for_status()
-print(response.status_code)
-
 
 data = response.json()['setlist'][0]['sets']['set']
 songsData = data[0]['song']
-songsData2 = data[1]['song']
-rangeSong = len(songsData)
-rangeEncore = len(songsData2)
-rangeAll = rangeEncore + rangeSong
-print(rangeAll)
-setlistAll = []
-for num in range(rangeSong):
-    setlistAll.append(songsData[num]['name'])
+try:  # try method if setlist has encore songs, if it has them, it appends them to a setlistAll list
+    songsData2 = data[1]['song']
+    rangeSong = len(songsData)
+    rangeEncore = len(songsData2)
+    rangeAll = rangeEncore + rangeSong
+    setlistAll = []
+    for num in range(rangeSong):
+        setlistAll.append(songsData[num]['name'])
 
-for num in range(rangeEncore):
-    setlistAll.append(songsData2[num]['name'])
+    for num in range(rangeEncore):
+        setlistAll.append(songsData2[num]['name'])
+    print(setlistAll)
+except IndexError:  # if an error is raised because there are no encore songs, only songs are passed to setlistAll list
+    rangeSong = len(songsData)
+    setlistAll = []
+    for num in range(rangeSong):
+        setlistAll.append(songsData[num]['name'])
+    print(setlistAll)
 
-print(setlistAll)
+
